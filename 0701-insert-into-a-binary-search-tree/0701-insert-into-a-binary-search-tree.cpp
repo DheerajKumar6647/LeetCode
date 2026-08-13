b2d08b944928f0752a2be129f8d50e23479bf7f6
DheerajKumar6647 <connecttodheerajkumar@gmail.com>
2026-08-14 00:51:55 +0530

    Time: 0 ms (100%), Space: 59.2 MB (93.74%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == NULL) return new TreeNode(val);
+        else if(val < root->val) {
+            if(root->left == NULL) {
+                root->left = new TreeNode(val);
+                return root;
+            }
+            else  insertIntoBST(root->left , val);
+        }
+        else {
+            if(root->right == NULL) {
+                root->right = new TreeNode(val);
+                return root;
+            }       
+            else  insertIntoBST(root->right , val);
+        }
+        return root;
+
+        
+    }
+};
